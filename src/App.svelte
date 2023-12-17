4fd937542e131ef3a98f069a19cafcb3059d7b6b
TheCubiq <jlajsek@gmail.com>
2023-12-18 00:33:20 +0100

    ✨Progress first steps basically.. will work on it more tomorrow xd

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,76 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+    import SvgDefines from "./lib/SvgDefines.svelte";
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+    <div class="img-main">
+        <svg class="ref-svg" height="100%" width="100%">
+            <mask id="meta-mask">
+                <use xlink:href="#masking-path" filter="url(#round)"></use>
+            </mask>
+        </svg>
+    </div>
 </main>
 
+<SvgDefines />
+
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+    @import url("https://fonts.googleapis.com/css2?family=Manrope");
+
+    main {
+        /*   max-width: 30rem; */
+        width: 100%;
+        max-width: 80rem;
+        height: 100%;
+
+        position: relative;
+        display: flex;
+
+        /*   resize: both;
+  overflow: auto; */
+
+        padding: 2rem;
+        /*   border-radius: 4.5rem; */
+        border-radius: 1rem;
+        border: 1px solid #1f1f37;
+
+        background: linear-gradient(
+                0deg,
+                rgba(9, 9, 10, 0) 0%,
+                rgba(9, 9, 10, 0) 100%
+            ),
+            url(https://i.imgur.com/2kGyIF8.png), no-repeat;
+
+        backdrop-filter: blur(24px);
+    }
+
+    svg {
+        position: absolute;
+        z-index: -1;
+        inset: 0;
+    }
+
+    symbol#masking-path {
+        fill: transparent;
+    }
+
+    .img-main {
+        height: 20rem;
+        width: 10rem;
+
+        max-width: 50rem;
+
+        position: relative;
+
+        flex: 1;
+
+        background-image: url(https://i.imgur.com/7JQsjw1.png);
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+
+        mask-image: url(#meta-mask);
+        mask-repeat: no-repeat;
+        webkit-mask-repeat: no-repeat;
+    }
 </style>
---
+++ src/lib/SvgDefines.svelte
@@ -0,0 +1,24 @@
+<svg xmlns="http://www.w3.org/2000/svg" height="1" width="1" viewBox="0 0 1 1">
+    <defs>
+      <!-- trying to round corners on the go: -->
+      <filter id='round'>
+        <feGaussianBlur id="blur-radius" in='SourceGraphic' stdDeviation='20' result='name' />
+        <feColorMatrix in='name' mode='matrix' values='1 0 0 0 0
+                  0 1 0 0 0 
+                  0 0 1 0 0
+                  0 0 0 30 -15' result='aab' />
+      </filter>
+      
+      <symbol id="masking-path" viewBox="0 0 837 650" preserveAspectRatio="none">
+        <path d="M40.0266 0.776855L629 28.7955V146.207H793.416L837 580.273L0 649.652L40.0266 0.776855Z" fill="white" />
+      </symbol>
+      
+      <!-- <animate
+        xlink:href="#blur-radius"
+        attributeName="stdDeviation"
+        values="0;20;0"
+        dur="1s"
+        repeatCount="indefinite" /> -->
+      
+    </defs>
+  </svg>
