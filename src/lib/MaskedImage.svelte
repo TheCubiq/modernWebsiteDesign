135d135d321f9c7a978c6961f47a407b5526f6a6
TheCubiq <jlajsek@gmail.com>
2023-12-19 22:56:05 +0100

    🔨 finally fixed the mask! gotta try it on safari tho..

--- src/lib/MaskedImage.svelte
+++ src/lib/MaskedImage.svelte
@@ -1,59 +1,25 @@
-<script>
-    import SvelteLogo from "../assets/svelte.svg";
-</script>
 
 <div class="mask-wrapper">
     <!-- <slot /> -->
     <div class="helper child"></div>
-
-
-    <svg xmlns="http://www.w3.org/2000/svg" class="child">
-        <!-- <defs> -->
-            <mask id="meta-mask" maskUnits="userSpaceOnUse" preserveAspectRatio="none">
-                <use href="#masking-path" xlink:href="#masking-path" 
-                ></use>
-                <!-- filter="url(#round)" -->
-            </mask>
-            <!-- <image href="https://i.imgur.com/TGQl5tZ.png" src="https://i.imgur.com/TGQl5tZ.png" width="100%" height="100%" preserveAspectRatio="none" /> -->
-        <!-- </defs> -->
-    </svg>
-
-    <!-- <svg xmlns="http://www.w3.org/2000/svg" class="child" preserveAspectRatio="none" height="100%" width="100%"> -->
-        <!-- <mask id="meta-mask" preserveAspectRatio="none"> -->
-            <!-- <use href="#masking-path" preserveAspectRatio="none" height="837" width="650" xlink:href="#masking-path" ></use> -->
-            <!-- <rect x="0" y="0" width="837" height="650" fill="url(#masking-path)"/> -->
-            <!-- filter="url(#round)" -->
-        <!-- </mask> -->
-        <!-- <image href="https://i.imgur.com/TGQl5tZ.png" src="https://i.imgur.com/TGQl5tZ.png" width="100%" height="100%" preserveAspectRatio="none" /> -->
-    <!-- </svg> -->
 </div>
 
 <style>
     .mask-wrapper {
-
         grid-column: span 2;
         grid-row: span 6;
-
-        /* height: 100%; */
-        /* position: relative; */
 
         display: grid;
         grid-template-columns: 1fr;
 
-        /* mask-image: url("#meta-mask"); */
-        mask-image: url("../assets/path.svg");
+        -webkit-mask-image: url("#masking-path");
+        mask-image: url("#masking-path");
 
         mask-repeat: no-repeat;
         mask-size: 100% 100%;
         -webkit-mask-clip: content-box;
         mask-clip: content-box;
-
-
-        /* position: relative; */
-
     }
-
-    
     .child {
         grid-row-start: 1;
         grid-column-start: 1;
@@ -61,22 +27,13 @@
         inline-size: 100%;
         block-size: 100%;
 
-        block-size: 100%;
-        /* min-block-size: 20vb; */
+        object-fit: cover;
 
-    object-fit: cover;
-
-place-self: stretch;
+        place-self: stretch;
     }
-    
-
-    .helper{
+    .helper {
         background-image: url("https://i.imgur.com/TGQl5tZ.png");
         background-size: cover;
         background-position: center;
-
     }
-    /* use {
-        filter: url(#round);
-    } */
 </style>
--- src/lib/SvgDefines.svelte
+++ src/lib/SvgDefines.svelte
@@ -1,22 +1,17 @@
-<svg class="def" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" >
+<svg class="def" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="0" height="0">
     <defs>
       <!-- trying to round corners on the go: -->
       <filter id='round'>
-        <feGaussianBlur id="blur-radius" in='SourceGraphic' stdDeviation='20' result='name' />
+        <feGaussianBlur id="blur-radius" in='SourceGraphic' stdDeviation='0.05' result='name' />
         <feColorMatrix in='name' mode='matrix' values='1 0 0 0 0
                   0 1 0 0 0 
                   0 0 1 0 0
-                  0 0 0 30 -15' result='aab' />
+                  0 0 0 80 -40' result='aab' />
       </filter>
       
-      <symbol id="masking-path" viewBox="0 0 837 650" preserveAspectRatio="none">
-        <path d="M40.0266 0.776855L629 28.7955V146.207H793.416L837 580.273L0 649.652L40.0266 0.776855Z" fill="white" />
-      </symbol>
-
-      <symbol viewBox="0 0 24 24" preserveAspectRatio="none" id="youtube" fill="white" >
-        <path d="M2.50001 17C1.80143 13.7033 1.80143 10.2967 2.50001 7C2.5918 6.66521 2.76914 6.36007 3.01461 6.11461C3.26008 5.86914 3.56522 5.69179 3.90001 5.6C9.26346 4.71146 14.7366 4.71146 20.1 5.6C20.4348 5.69179 20.7399 5.86914 20.9854 6.11461C21.2309 6.36007 21.4082 6.66521 21.5 7C22.1986 10.2967 22.1986 13.7033 21.5 17C21.4082 17.3348 21.2309 17.6399 20.9854 17.8854C20.7399 18.1309 20.4348 18.3082 20.1 18.4C14.7366 19.2887 9.26344 19.2887 3.90001 18.4C3.56522 18.3082 3.26008 18.1309 3.01461 17.8854C2.76914 17.6399 2.5918 17.3348 2.50001 17Z" />
-        <path d="M10 15L15 12L10 9V15Z" />
-      </symbol>
+      <mask id="masking-path" maskUnits="objectBoundingBox" maskContentUnits="objectBoundingBox">
+        <path d="M.0478.0013.7512.0443V.459H.9477L.9996.8931 0 1 .0478.0013Z" fill="white" filter="url(#round)" />
+      </mask>
       
       <!-- <animate
         xlink:href="#blur-radius"
