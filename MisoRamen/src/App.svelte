f927fef617b5f9715596b81c2abac3d234ba1b7b
TheCubiq <jlajsek@gmail.com>
2024-01-07 21:06:35 +0100

    ✨ Update ♒ Font 🌈 Colors 💅 Layout 🧮 Structurize

--- MisoRamen/src/App.svelte
+++ MisoRamen/src/App.svelte
@@ -1,66 +1,55 @@
 <script>
 </script>
 
-<main>
+<main role="main">
   <header>
-    <p>logo</p>
+    <a href="/">logo</a>
     <p>path</p>
-    <p>search/cart</p>
+    <p>search</p>
+    <p>cart</p>
     <p>happi</p>
   </header>
-  <nav>nav</nav>
+  <nav>
+    <a href="">about</a>
+    <a href="">menu</a>
+    <a href="">shop</a>
+    <a href="">order</a>
+  </nav>
   <section>
     <article>main</article>
-    <article>
+    <aside>
       <!-- ingredients -->
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa corporis blanditiis est excepturi quam optio repellat quae reprehenderit reiciendis! Optio officiis, nobis fuga minima sed maiores fugiat, reprehenderit magni, nesciunt adipisci unde cumque sequi incidunt quasi quas porro vitae! Corporis sint deserunt provident eius voluptates eligendi rerum, totam quisquam dicta!
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa corporis blanditiis est excepturi quam optio repellat quae reprehenderit reiciendis! Optio officiis, nobis fuga minima sed maiores fugiat, reprehenderit magni, nesciunt adipisci unde cumque sequi incidunt quasi quas porro vitae! Corporis sint deserunt provident eius voluptates eligendi rerum, totam quisquam dicta!
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa corporis blanditiis est excepturi quam optio repellat quae reprehenderit reiciendis! Optio officiis, nobis fuga minima sed maiores fugiat, reprehenderit magni, nesciunt adipisci unde cumque sequi incidunt quasi quas porro vitae! Corporis sint deserunt provident eius voluptates eligendi rerum, totam quisquam dicta!
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa corporis blanditiis est excepturi quam optio repellat quae reprehenderit reiciendis! Optio officiis, nobis fuga minima sed maiores fugiat, reprehenderit magni, nesciunt adipisci unde cumque sequi incidunt quasi quas porro vitae! Corporis sint deserunt provident eius voluptates eligendi rerum, totam quisquam dicta!
-    </article>
+      Lorem ipsum, dolor sit amet reiciendis! Optio officiis, nobis fuga minima sed
+      maiores fugiat, reprehenderit magni, nesciunt adipisci unde cumque sequi incidunt
+      quasi quas porro vitae! Corporis sint deserunt provident eius voluptates eligendi
+      rerum, totam quisquam dicta!
+    </aside>
   </section>
 </main>
 
 <style>
   main {
+    font-size: 1.5rem;
+    
+    text-align: start;
+
     flex: 1;
     display: grid;
-    place-items: center;
-    grid-template-columns: auto 4fr 1fr auto;
+    /* place-items: center; */
+    grid-template-columns: auto 8fr 1fr 1fr auto;
     grid-template-rows: auto 1fr;
 
-    max-height: 100vh;
+    max-height: 100svh;
 
     /* gap:2px; */
-    background-color: black;
   }
 
-  main *:not(:is(header,section)) {
+  main > *:not(:is(header, section)) {
     width: 100%;
     height: 100%;
     /* border: 1px solid red; */
-    
+
     /* background-color: rgb(66, 66, 66); */
-  }
-
-  main > * {
-  }
-
-  nav {
-    /* display: grid; */
-    /* grid-template-columns: subgrid; */
-    /* grid-row: 1/-1;
-    grid-column: 1/2; */
-    border-right: 1px solid white;
-
-    /* position: sticky; */
-    /* top:0; */
-
-    /* max-height: 100svh; */
-
-    /* display: flex; */
-
-    /* align-items: center; */
   }
 
   header {
@@ -72,21 +61,43 @@
     position: sticky;
     bottom: 100%;
 
-    border-bottom: 1px solid white;
+    border-bottom: 1px solid var(--clr-text);
   }
 
-  header > * {
-    border-right: 1px solid white;
+  header > *:not(:last-child),
+  nav {
+    border-right: 1px solid var(--clr-text);
+
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
   }
 
   section {
     display: grid;
     grid-template-columns: subgrid;
-    grid-column: span 3;
+    grid-column: 2/-1;
+
+    /* place-content: center; */
+
 
     overflow-x: hidden;
-    overflow-y: scroll;
-    
+    overflow-y: auto;
+
     height: 100%;
+
+    padding: 1em;
+  }
+  
+  article {
+    /* position: sticky; */
+    /* position: fixed; */
+    /* top: auto; */
+    z-index: 5;
+  }
+
+  aside {
+    overflow-y: auto;
+    grid-column: -4/span 2;
   }
 </style>
