9b3f89cdabf7c10324e53ffe66a03f4490f884c5
TheCubiq <jlajsek@gmail.com>
2024-01-10 13:39:28 +0100

    📤 Header update

--- AiAssistant/src/App.svelte
+++ AiAssistant/src/App.svelte
@@ -4,15 +4,19 @@
 <main>
 
   <header>
-    <svg xmlns="http://www.w3.org/2000/svg" width="47" height="19" fill="none">
-      <g fill="currentColor">
-        <path
-          d="M32.698 1.162A2.375 2.375 0 0 1 34.74 0h7.539c1.84 0 2.982 2.004 2.042 3.588l-8.461 14.25A2.375 2.375 0 0 1 33.818 19h-7.539c-1.84 0-2.982-2.004-2.042-3.588l8.461-14.25ZM20.823 1.162A2.375 2.375 0 0 1 22.865 0h3.976c1.841 0 2.982 2.004 2.042 3.588l-8.46 14.25A2.375 2.375 0 0 1 18.38 19h-3.976c-1.84 0-2.982-2.004-2.042-3.588l8.461-14.25ZM11.323 1.162A2.375 2.375 0 0 1 13.365 0h1.601c1.841 0 2.982 2.004 2.042 3.588l-8.46 14.25A2.375 2.375 0 0 1 6.504 19h-1.6c-1.842 0-2.983-2.004-2.043-3.588l8.461-14.25Z" />
-      </g>
-    </svg>
-    <a href="/">subscription</a>
-    <a href="/">the finals released</a>
-    <a href="/">en</a>
+    <nav class="left">
+      <svg xmlns="http://www.w3.org/2000/svg" width="47" height="19" fill="none">
+        <g fill="currentColor">
+          <path
+            d="M32.698 1.162A2.375 2.375 0 0 1 34.74 0h7.539c1.84 0 2.982 2.004 2.042 3.588l-8.461 14.25A2.375 2.375 0 0 1 33.818 19h-7.539c-1.84 0-2.982-2.004-2.042-3.588l8.461-14.25ZM20.823 1.162A2.375 2.375 0 0 1 22.865 0h3.976c1.841 0 2.982 2.004 2.042 3.588l-8.46 14.25A2.375 2.375 0 0 1 18.38 19h-3.976c-1.84 0-2.982-2.004-2.042-3.588l8.461-14.25ZM11.323 1.162A2.375 2.375 0 0 1 13.365 0h1.601c1.841 0 2.982 2.004 2.042 3.588l-8.46 14.25A2.375 2.375 0 0 1 6.504 19h-1.6c-1.842 0-2.983-2.004-2.043-3.588l8.461-14.25Z" />
+        </g>
+      </svg>
+      <a href="/">subscription</a>
+      <a href="/">the finals released</a>
+    </nav>
+    <nav class="right">
+      <a href="/">en</a>
+    </nav>
   </header>
 
   <section id="hero" class="corners">
@@ -40,11 +44,12 @@
     grid-template-rows: auto 1fr;
 
     padding: 1.5rem;
-    gap: 1.5rem;
+    column-gap: var(--spacing);
+    row-gap: var(--spacing-half);
 
   }
 
-  main > *{
+  main > section{
     background-color: var(--clr-secondary);
     width: 100%;
     height: 100%;
@@ -57,17 +62,56 @@
     grid-row: 1;
     display: flex;
     justify-content: space-between;
-    align-items: center;
-    padding: 1rem;
-    background-color: var(--clr-secondary);
+
+    align-items: flex-start;
 
     z-index: 999;
     
     border-radius: var(--corner-radius);
+
+    gap: var(--spacing);
   }
 
-  header > * {
+  /* header > * {
     background-color: var(--clr-bg);
+  } */
+
+  header > nav {
+    display: flex;
+    /* justify-content: space-between; */
+    align-items: center;
+
+    flex-basis: 50%;
+
+    flex-wrap: wrap;
+    justify-content: center;
+    column-gap: var(--spacing-half);
+  }
+
+
+
+  header > nav.left > :last-child {
+    /* justify-content: flex-start; */
+    display: flex;
+    justify-self: flex-end;
+  }
+
+  header > nav.left > a:nth-last-child(2) {
+    display: flex;
+    flex:1;
+  }
+
+
+
+  header > nav.right {
+    justify-content: flex-end;
+  }
+
+  nav > a {
+    padding: .5em ;
+    background-color: var(--clr-secondary);
+    line-height: 1;
+    border-radius: 99em;
   }
 
   section {
