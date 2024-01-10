e86b47a0a78594c09321528b03fc480ba120d5d8
TheCubiq <jlajsek@gmail.com>
2024-01-10 17:02:23 +0100

    ✨ Progress 🧮 Structure 💅 Layout ➰ Responsivity
    holy shit grid areas is so damn powerful :o

--- AiAssistant/src/App.svelte
+++ AiAssistant/src/App.svelte
@@ -2,32 +2,38 @@
 </script>
 
 <main>
-
   <header>
-    <nav class="left">
-      <svg xmlns="http://www.w3.org/2000/svg" width="47" height="19" fill="none">
-        <g fill="currentColor">
-          <path
-            d="M32.698 1.162A2.375 2.375 0 0 1 34.74 0h7.539c1.84 0 2.982 2.004 2.042 3.588l-8.461 14.25A2.375 2.375 0 0 1 33.818 19h-7.539c-1.84 0-2.982-2.004-2.042-3.588l8.461-14.25ZM20.823 1.162A2.375 2.375 0 0 1 22.865 0h3.976c1.841 0 2.982 2.004 2.042 3.588l-8.46 14.25A2.375 2.375 0 0 1 18.38 19h-3.976c-1.84 0-2.982-2.004-2.042-3.588l8.461-14.25ZM11.323 1.162A2.375 2.375 0 0 1 13.365 0h1.601c1.841 0 2.982 2.004 2.042 3.588l-8.46 14.25A2.375 2.375 0 0 1 6.504 19h-1.6c-1.842 0-2.983-2.004-2.043-3.588l8.461-14.25Z" />
-        </g>
-      </svg>
-      <a href="/">subscription</a>
-      <a href="/">the finals released</a>
-    </nav>
-    <nav class="right">
-      <a href="/">en</a>
-    </nav>
+    <ul class="left">
+      <li>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="47"
+          height="19"
+          fill="none"
+        >
+          <g fill="currentColor">
+            <path
+              d="M32.698 1.162A2.375 2.375 0 0 1 34.74 0h7.539c1.84 0 2.982 2.004 2.042 3.588l-8.461 14.25A2.375 2.375 0 0 1 33.818 19h-7.539c-1.84 0-2.982-2.004-2.042-3.588l8.461-14.25ZM20.823 1.162A2.375 2.375 0 0 1 22.865 0h3.976c1.841 0 2.982 2.004 2.042 3.588l-8.46 14.25A2.375 2.375 0 0 1 18.38 19h-3.976c-1.84 0-2.982-2.004-2.042-3.588l8.461-14.25ZM11.323 1.162A2.375 2.375 0 0 1 13.365 0h1.601c1.841 0 2.982 2.004 2.042 3.588l-8.46 14.25A2.375 2.375 0 0 1 6.504 19h-1.6c-1.842 0-2.983-2.004-2.043-3.588l8.461-14.25Z"
+            />
+          </g>
+        </svg>
+      </li>
+      <li>
+        <a href="/">subscription</a>
+      </li>
+      <li>
+        <a href="/">the finals released</a>
+      </li>
+    </ul>
+    <ul class="right">
+      <li>
+        <a href="/">en</a>
+      </li>
+    </ul>
   </header>
 
-  <section id="hero" class="corners">
-    ai gen.2
-  </section>
-  <section id="slideshow">
-    img
-  </section>
-
-
-
+  <section id="hero" class="corners">ai gen.2</section>
+  <section id="slideshow">img</section>
 </main>
 
 <style>
@@ -40,16 +46,35 @@
     /* rad:2px; */
     background-color: var(--clr-bg);
 
-    grid-template-columns: 1fr 1fr;
-    grid-template-rows: auto 1fr;
+    /* grid-template-columns: 1fr 1fr;
+    grid-template-rows: auto 1fr; */
+
+    /* grid-template-columns: [header-start] 1fr [slideshow-start] 1fr [header-end slideshow-end];
+    grid-template-rows: [header-start slideshow-start] auto [header-end] 1fr [slideshow-end]; */
+
+    grid-template-columns: [header-start hero-start slideshow-start] 1fr [header-end hero-end slideshow-end];
+    grid-template-rows: 
+      [header-start] auto 
+      [header-end hero-start] 1fr 
+      [hero-end slideshow-start] 1fr [slideshow-end];
 
     padding: 1.5rem;
     column-gap: var(--spacing);
     row-gap: var(--spacing-half);
-
   }
 
-  main > section{
+  @media (min-width: 48rem) {
+    main {
+      grid-template-columns: 
+        [header-start hero-start] 1fr 
+        [hero-end slideshow-start] 1fr [header-end slideshow-end];
+      grid-template-rows: 
+        [header-start slideshow-start] auto 
+        [header-end hero-start] 1fr [hero-end slideshow-end];
+    }
+  }
+
+  main > section {
     background-color: var(--clr-secondary);
     width: 100%;
     height: 100%;
@@ -58,15 +83,19 @@
   header {
     width: 100%;
     height: auto;
-    grid-column: 1 / -1;
-    grid-row: 1;
+
+    /* grid-column: 1 / -1;
+    grid-row: 1; */
+
+    grid-area: header;
+
     display: flex;
     justify-content: space-between;
 
     align-items: flex-start;
 
     z-index: 999;
-    
+
     border-radius: var(--corner-radius);
 
     gap: var(--spacing);
@@ -76,7 +105,7 @@
     background-color: var(--clr-bg);
   } */
 
-  header > nav {
+  header > ul {
     display: flex;
     /* justify-content: space-between; */
     align-items: center;
@@ -84,34 +113,55 @@
     flex-basis: 50%;
 
     flex-wrap: wrap;
-    justify-content: center;
+    justify-content: space-evenly;
     column-gap: var(--spacing-half);
+
+    list-style: none;
   }
 
-
-
-  header > nav.left > :last-child {
+  header > ul.left > :last-child {
     /* justify-content: flex-start; */
     display: flex;
     justify-self: flex-end;
   }
 
-  header > nav.left > a:nth-last-child(2) {
+  header > ul.left > :last-child {
     display: flex;
-    flex:1;
-  }
-
-
-
-  header > nav.right {
+    flex: 1;
     justify-content: flex-end;
   }
 
-  nav > a {
-    padding: .5em ;
+
+
+  header > ul.right {
+    justify-content: flex-end;
+  }
+
+  ul a {
+    padding: 0.5em;
     background-color: var(--clr-secondary);
+    /* background-color: red; */
     line-height: 1;
-    border-radius: 99em;
+    border-radius: 1em;
+
+    text-wrap: nowrap;
+  }
+
+  ul li {
+    display: flex;
+
+    /* background: blue; */
+    background: var(--clr-bg);
+    padding: 0.5em;
+  }
+
+  .left li {
+    border-radius: var(--corner-radius);
+    padding-inline: 0;
+  }
+
+  .right li {
+    border-bottom-left-radius: .75em;
   }
 
   section {
@@ -122,9 +172,16 @@
     border-radius: var(--corner-radius);
   }
 
+  #hero {
+
+    grid-area: hero;
+  }
+
   #slideshow {
-    grid-column: 2;
-    grid-row: 1 / -1;
+    /* grid-column: 2;
+    grid-row: 1 / -1; */
+
+    grid-area: slideshow;
 
     position: relative;
 
@@ -142,52 +199,46 @@
     /* z-index: -1; */
   }
 
-
   .corners {
-
     position: relative;
-    
+
     z-index: 0;
   }
 
   .corners::after {
-  content: "";
-  position: absolute;
-  inset: 0em;
-  z-index: -1;
-  border-radius: var(--corner-radius);
-  background: inherit;
+    content: "";
+    position: absolute;
+    inset: 0em;
+    z-index: -1;
+    border-radius: var(--corner-radius);
+    background: inherit;
 
-  transition: 0.1s;
-}
+    transition: 0.1s;
+  }
 
   .corners::before {
-  content: "";
-  position: absolute;
+    content: "";
+    position: absolute;
 
-  --stroke-width: 0.17em;
-  inset: calc(var(--stroke-width) * -1);
+    --stroke-width: 0.17em;
+    inset: calc(var(--stroke-width) * -1);
 
-  --radi: calc(var(--corner-radius) + var(--stroke-width));
-  border-radius: var(--radi);
+    --radi: calc(var(--corner-radius) + var(--stroke-width));
+    border-radius: var(--radi);
 
-  --over-length: 20px;
-  --off-radi: calc(var(--radi) + var(--over-length));
+    --over-length: 20px;
+    --off-radi: calc(var(--radi) + var(--over-length));
 
-  --circ: var(--clr-primary) var(--off-radi), transparent var(--off-radi);
+    --circ: var(--clr-primary) var(--off-radi), transparent var(--off-radi);
 
-  background: radial-gradient(circle at left top, var(--circ)),
-    radial-gradient(circle at right top, var(--circ)),
-    radial-gradient(circle at left bottom, var(--circ)),
-    radial-gradient(circle at right bottom, var(--circ));
-  z-index: -2;
+    background: radial-gradient(circle at left top, var(--circ)),
+      radial-gradient(circle at right top, var(--circ)),
+      radial-gradient(circle at left bottom, var(--circ)),
+      radial-gradient(circle at right bottom, var(--circ));
+    z-index: -2;
 
-  scale: 1;
-}
-
-
-
-
+    scale: 1;
+  }
 
   /* background: linear-gradient(
     0deg,
