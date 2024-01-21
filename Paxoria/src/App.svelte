654f9b90e36f497a9e58a44c48282ad378ef51a7
TheCubiq <jlajsek@gmail.com>
2024-01-21 22:36:35 +0100

    ✨Init Paxoria New Week, New Project

---
+++ Paxoria/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+</script>
+
+<main>
+  do stuff there
+</main>
+
+<style>
+  main {
+    flex: 1;
+    display: grid;
+    place-items: center;
+    max-height: 100vh;
+
+
+    /* gap:2px; */
+    background-color: var(--clr-bg);
+  }
+</style>
