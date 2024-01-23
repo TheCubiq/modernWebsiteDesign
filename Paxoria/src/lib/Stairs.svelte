4b8480bef07fb721346beff79826af24987115af
TheCubiq <jlajsek@gmail.com>
2024-01-23 15:57:23 +0100

    🤏 Stairs

--- Paxoria/src/lib/Stairs.svelte
+++ Paxoria/src/lib/Stairs.svelte
@@ -4,25 +4,30 @@
   import { M, Motion, MotionDiv } from "svelte-motion";
   export let nbOfColumns = 5;
 
+  export let animDefines = {
+    delay: .05,
+    duration: .3,
+  }
+
   const anim = (variants, custom) => {
     return {
       initial: "initial",
       animate: "enter",
       exit: "exit",
       variants,
-      custom
+      custom,
     };
   };
 
-  const expand ={
+  const expand = {
     initial: {
       top: 0,
     },
     enter: (i) => ({
       top: "100%",
       transition: {
-        duration: .4,
-        delay: i * 0.1,
+        duration: animDefines.duration,
+        delay: i * animDefines.delay,
       },
       transitionEnd: {
         height: 0,
@@ -32,16 +37,30 @@
     exit: (i) => ({
       height: "100%",
       transition: {
-        duration: .4,
-        delay: i * 0.1,
+        duration: animDefines.duration,
+        delay: i * animDefines.delay,
       },
-    })
+    }),
+  };
 
-  }
-
+  const opacity = {
+    initial: {
+      opacity: 0.5,
+    },
+    enter: {
+      opacity: 0,
+    },
+    exit: {
+      opacity: .5,
+    },
+  };
 </script>
 
 <div class="page">
+  <Motion let:motion={m_opacity} {...anim(opacity)}>
+    <aside use:m_opacity class="background"></aside>
+  </Motion>
+
   <div class="transition-container">
     {#each Array(nbOfColumns) as _, i}
       <Motion let:motion={m_expand} {...anim(expand, i)}>
@@ -67,6 +86,18 @@
     flex-direction: row-reverse;
 
     display: flex;
+  }
+
+  aside.background {
+    position: fixed;
+
+    height: 100vh;
+    width: 100vw;
+
+    left: 0;
+    top: 0;
+    pointer-events: none;
+    background-color: black;
   }
 
   .transition-container > aside {
