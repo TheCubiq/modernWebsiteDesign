96eca938ebd11d765fc51b61d04efb317fcab22a
TheCubiq <jlajsek@gmail.com>
2024-01-25 20:14:25 +0100

    🧪 smooth scrolling animation

--- Paxoria/src/App.svelte
+++ Paxoria/src/App.svelte
@@ -9,6 +9,9 @@
   import Curve from "./lib/Curve.svelte";
   import { fade, fly } from "svelte/transition";
   import StairsNative from "./lib/StairsNative.svelte";
+  import { flip } from "svelte/animate";
+  import { onMount } from "svelte";
+  import { quintInOut } from "svelte/easing";
 
   let page = "Simpsons";
 
@@ -18,76 +21,86 @@
     StarWars,
   };
 
-  const sections = {
-    "Arc Browser": {
-      Simpsons,
-      Futurama,
-      StarWars,
-    },
-    "Chromium": {
-      Simpsons,
-      Futurama,
-      StarWars,
-    },
-  }
+  const sections = [
+    "Works",
+    "F37",
+    "Nomad",
+    "DeepMind",
+    "Notice",
+    "Easings",
+    "Want",
+    "Koto",
+    "OnSite",
+    "10x",
+    "Follow",
+    "Photographs",
+    "Thread",
+    // "SuperHi",
+    // "Nonagrams",
+    // "IO",
+    // "Kindeo",
+    // "INT",
+    // "Airbnb",
+    // "Google",
+    // "Archive",
+  ];
 
+  const repeatArray = (arr, times) => {
+    let result = [];
+    for (let i = 0; i < times; i++) {
+      result = result.concat(arr);
+    }
+    return result;
+  };
+
+  const intervalDelay = 2000;
+  let counter = 0;
+
+  const transitionBase = (i) => {
+    return {
+      duration: 1000,
+      delay: i * 50,
+      easing: quintInOut,
+    };
+  };
+
+  onMount(() => {
+    const interval = setInterval(() => {
+      // counter = counter + Math.floor(((Math.random() - 0.5) * 2) * 2);
+      counter = counter + 2;
+    }, intervalDelay);
+
+    return () => clearInterval(interval);
+  });
 </script>
 
 <main>
-  <!-- <AnimatePresence list={[{ key: page }]} let:item exitBeforeEnter>
-    <Inner>
-      <article>
-        <nav>
-          {#each Object.entries(pages) as [key], i}
-            <button on:click={() => (page = key)}>{key}</button>
-          {/each}
-        </nav>
-        <svelte:component this={pages[item.key]} />
-      </article>
-    </Inner>
-  </AnimatePresence> -->
-
   <!-- <nav>
     {#each Object.entries(pages) as [key], i}
       <button on:click={() => (page = key)}>{key}</button>
     {/each}
-  </nav>
-  <AnimatePresence list={[{ key: page }]} let:item exitBeforeEnter>
-    <Stairs nbOfColumns={5}>
-      <article>
-        <svelte:component this={pages[item.key]} />
-      </article>
-    </Stairs>
-  </AnimatePresence> -->
+  </nav> -->
 
-  <!-- <nav>
-    {#each Object.entries(pages) as [key], i}
-      <button on:click={() => (page = key)}>{key}</button>
-    {/each}
-  </nav>
-  <AnimatePresence list={[{ key: page }]} let:item exitBeforeEnter>
-    <Curve title={item.key}>
-      <article>
-        <svelte:component this={pages[item.key]} />
-      </article>
-    </Curve>
-  </AnimatePresence> -->
-
-  <nav>
-    {#each Object.entries(pages) as [key], i}
-      <button on:click={() => (page = key)}>{key}</button>
+  <nav class="entries">
+    {#each sections as section, i (i + counter)}
+      <span
+        animate:flip={{
+          ...transitionBase(i + 5),
+        }}
+        out:fly={{ x: -100, ...transitionBase(i) }}
+        in:fly={{ x: -100, ...transitionBase(i + 5) }}
+        >{sections[(i + counter) % sections.length]}</span
+      >
     {/each}
   </nav>
 
-  {#key page}
+  <!-- {#key page}
     <StairsNative nbOfColumns={10}>
       <article>
         <svelte:component this={pages[page]} />
       </article>
     </StairsNative>
-  {/key}
-
-  <button>test</button>
+  {/key} -->
 </main>
 
 <style>
@@ -99,10 +112,25 @@
     background-color: var(--clr-bg);
   }
 
-  main nav {
+  /* main nav {
     position: sticky;
     top: 0;
     margin-block: 3em;
+  } */
+
+  nav.entries {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+    overflow: hidden;
+
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+
+    font-size: 2.5rem;
   }
 
   article {
