f8ad7eee118fe6f6e1dbe8e24a27f250b5906e85
TheCubiq <jlajsek@gmail.com>
2024-02-14 16:37:38 +0100

    🎉 Init empty project

--- TokyoFestival/src/lib/MaskedImage.svelte
+++ TokyoFestival/src/lib/MaskedImage.svelte
@@ -5,15 +5,18 @@
 
 <style>
     .mask-wrapper {
-        grid-column: span 2;
-        grid-row: span 6;
+        /* grid-column: span 2;
+        grid-row: span 6; */
 
-        -webkit-mask-image: url("#masking-path");
-        mask-image: url("#masking-path");
+        /* -webkit-mask-image: url("#masking-path");
+        mask-image: url("#masking-path"); */
+        /* -webkit-mask: url("#masking-path"); */
+        /* mask: url("#masking-path"); */
+        mask: url("../assets/maskingPath1.svg")
 
-        mask-repeat: no-repeat;
+        /* mask-repeat: no-repeat;
         mask-size: 100% 100%;
         -webkit-mask-clip: content-box;
-        mask-clip: content-box;
+        mask-clip: content-box; */
     }    
 </style>
--- TokyoFestival/src/lib/SvgDefines.svelte
+++ TokyoFestival/src/lib/SvgDefines.svelte
@@ -1,36 +1,89 @@
-<svg class="def" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="0" height="0">
-    <defs>
-      <!-- trying to round corners on the go: -->
-      <filter id='round'>
-        <feGaussianBlur id="blur-radius" in='SourceGraphic' stdDeviation='0.04' result='name' />
-        <feColorMatrix in='name' mode='matrix' values='1 0 0 0 0
+<svg
+  class="def"
+  xmlns="http://www.w3.org/2000/svg"
+  xmlns:xlink="http://www.w3.org/1999/xlink"
+  width="0"
+  height="0"
+>
+  <defs>
+    <!-- trying to round corners on the go: -->
+    <filter id="round">
+      <feGaussianBlur
+        id="blur-radius"
+        in="SourceGraphic"
+        stdDeviation="0.04"
+        result="name"
+      />
+      <feColorMatrix
+        in="name"
+        mode="matrix"
+        values="1 0 0 0 0
                   0 1 0 0 0 
                   0 0 1 0 0
-                  0 0 0 80 -40' result='aab' />
-      </filter>
-
-      <filter id="glow" filterUnits="userSpaceOnUse">
-        <feMorphology in="mask" result="dilated" operator="dilate" radius="10"></feMorphology>
-        <feGaussianBlur in="dilated" result="blurred" stdDeviation="20"></feGaussianBlur>
-        <feMerge>
-            <feMergeNode in="blurred"></feMergeNode>
-            <feMergeNode in="SourceGraphic"></feMergeNode>
-        </feMerge>
+                  0 0 0 80 -40"
+        result="aab"
+      />
     </filter>
-      
-      <mask id="masking-path" maskUnits="objectBoundingBox" maskContentUnits="objectBoundingBox">
-        <path d="M.0478.0013.7512.0443V.245H.9477L.9996.8931 0 1 .0478.0013Z" fill="white" filter="url(#round)" />
-      </mask>
-      <mask id="blob2" maskUnits="objectBoundingBox" maskContentUnits="objectBoundingBox">
-        <path d="M1 0H.15V.15H0V1H1Z" fill="white" filter="url(#round)" />
-      </mask>
-      
-      <!-- <animate
+
+    <filter id='goo'>
+      <feGaussianBlur in='SourceGraphic' stdDeviation='0.04' result='name' />
+      <feColorMatrix in='name' mode='matrix' values='1 0 0 0 0
+                0 1 0 0 0 
+                0 0 1 0 0
+                0 0 0 80 -40 ' result='aab' />
+<!--       <feBlend in='SourceGraphic' in2='aab' /> -->
+      <feComposite in="SourceGraphic" in2="aab" operator="atop"/>
+    </filter>
+
+    <filter id="glow" filterUnits="userSpaceOnUse">
+      <feMorphology in="mask" result="dilated" operator="dilate" radius="10"
+      ></feMorphology>
+      <feGaussianBlur in="dilated" result="blurred" stdDeviation="20"
+      ></feGaussianBlur>
+      <feMerge>
+        <feMergeNode in="blurred"></feMergeNode>
+        <feMergeNode in="SourceGraphic"></feMergeNode>
+      </feMerge>
+    </filter>
+
+    <mask
+    id="masking-path"
+    maskContentUnits="objectBoundingBox"
+    >
+
+      <!-- <rect width="1" height="1" fill="url(#gradientSVG)" />
+      <linearGradient x2="0" y2="1" id="gradientSVG">
+        <stop offset="25%" stop-color="white" />
+        <stop offset="50%" stop-color="black" />
+      </linearGradient> -->
+      <path
+        d="M.0478.0013.7512.0443V.245H.9477L.9996.8931 0 1 .0478.0013Z"
+        fill="white"
+        filter="url(#round)"
+      />
+    </mask>
+    <mask
+    id="blob2"
+    maskUnits="objectBoundingBox"
+    maskContentUnits="objectBoundingBox"
+    >
+      <path d="M1 0H.15V.15H0V1H1Z" fill="white" filter="url(#round)" />
+    </mask>
+
+    <!-- <animate
         xlink:href="#blur-radius"
         attributeName="stdDeviation"
-        values="0;20;0"
-        dur="1s"
+        values="0;0.08;0"
+        dur="3s"
         repeatCount="indefinite" /> -->
-      
-    </defs>
-  </svg>
+  </defs>
+</svg>
+
+<style>
+  svg {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    pointer-events: none;
+  }
+</style>
