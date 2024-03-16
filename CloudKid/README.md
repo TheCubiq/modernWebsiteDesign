697469c824b33024dfa957fec86487ade849cc42
TheCubiq <jlajsek@gmail.com>
2024-03-16 23:05:06 +0100

    CloudKid init this one might be fun, couldn't do much today as I'm sick..  more stuff coming soon <3

---
+++ CloudKid/README.md
@@ -0,0 +1,5 @@
+Reference:
+
+[Angles by  Pixflow | Dribbble](https://dribbble.com/shots/23384310-Angles)
+
+![ref image](https://cdn.dribbble.com/userupload/12312201/file/original-dcfbc7b775da2900a2fafcdc091fbd94.jpg?resize=1600x1200)
---
+++ CloudKid/src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  import CloudKidFace from '/src/assets/cloud-kid-face.svg'
+</script>
+
+<main>
+  <img src={CloudKidFace} alt="CloudKid Face" />
+</main>
+
+<style>
+  main {
+    flex: 1;
+
+    display: grid;
+    place-items: center;
+  }
+
+  img {
+    height: 1rem;
+  }
+</style>
