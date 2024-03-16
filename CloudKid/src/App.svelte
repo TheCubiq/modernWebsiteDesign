0620ee29f7fabf9aa07ddf40b5e559c016a6fba6
TheCubiq <jlajsek@gmail.com>
2024-03-16 23:52:30 +0100

    ☁ Cute little liquid ✨Intro Background Loading
    ..Basically, no canvas :)

--- CloudKid/src/App.svelte
+++ CloudKid/src/App.svelte
@@ -1,10 +1,27 @@
 <script>
-  import CloudKidFace from '/src/assets/cloud-kid-face.svg'
+  import SvgDefines from "./lib/svgDefines.svelte";
+  import CloudKidFace from "/src/assets/cloud-kid-face.svg";
+
+  let isLoaded = false;
+
+  function handleLoad() {
+    isLoaded = true;
+  }
+
 </script>
 
 <main>
-  <img src={CloudKidFace} alt="CloudKid Face" />
+  <img
+    on:load={handleLoad}
+    class:intro={isLoaded}
+    class="full"
+    src="https://source.unsplash.com/random/1960x1080/?liquid"
+    alt="background"
+  />
+  <img class="logo" src={CloudKidFace} alt="CloudKid Face" />
 </main>
+
+<SvgDefines />
 
 <style>
   main {
@@ -12,6 +29,63 @@
 
     display: grid;
     place-items: center;
+
+    grid-template-areas: "stack";
+
+    position: relative;
+
+    overflow: hidden;
+  }
+
+  main * {
+    grid-area: stack;
+  }
+
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+
+  .intro {
+    animation: fadeIn 2s forwards;
+  }
+
+  .full {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    z-index: 0;
+    opacity: 0;
+  }
+
+  .logo {
+    mix-blend-mode: difference;
+    z-index: 2;
+  }
+
+  @keyframes rotate {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+  main::before {
+    content: "";
+    position: absolute;
+    inset: -50vmax;
+    -webkit-filter: url(#distort);
+    filter: url(#distort);
+    animation: rotate 50s linear infinite;
+    transform-origin: center 60%;
+    -webkit-backdrop-filter: blur(0);
+    backdrop-filter: blur(0);
+    z-index: 1;
   }
 
   img {
---
+++ CloudKid/src/lib/svgDefines.svelte
@@ -0,0 +1,28 @@
+<svg>
+  <defs>
+    <filter id="distort">
+      <feTurbulence
+        id="turbo"
+        type="fractalNoise"
+        numOctaves="1"
+        result="warp"
+        baseFrequency="0.015"
+      />
+      <feDisplacementMap
+        xChannelSelector="R"
+        yChannelSelector="G"
+        scale="30"
+        in="SourceGraphic"
+        in2="warp"
+      />
+    </filter>
+  </defs>
+</svg>
+
+<style>
+  svg {
+   position: absolute;
+   /* visibility: hidden; */
+   pointer-events: none;
+  }
+</style>
