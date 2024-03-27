a90b38e0d3faef1474219bd9d080bdda93bc55a7
TheCubiq <jlajsek@gmail.com>
2024-03-27 04:20:33 +0100

    👁 Hyperfocus progress 💀 Wrote like 90% of the whole project within few hrs ☠ Then realized that it's already 3am 👁 Focusable / hoverable /work/ buttons ↩ Switchable 3d (on hover) 👀 A lot of hover states

--- SplineWithSvelte/src/App.svelte
+++ SplineWithSvelte/src/App.svelte
@@ -2,63 +2,247 @@
   import { Application } from "@splinetool/runtime";
   import { onMount } from "svelte";
 
-  import scene from "./lib/spline/iphoneScreen.spline?url";
+  import CubiqLogo from "./assets/cubiqLogo.svg?raw";
+  import scene from "./assets/spline/iphoneScreen.spline?url";
+  import WorkPill from "./lib/workPill.svelte";
+  // import demoScreen from "./assets/demoScreen.png";
+  import { fade } from "svelte/transition";
 
   let canvas;
 
   let click;
+  let switchScene;
 
-  let sceneNo = 0;
+  let sceneNo = 1;
+
+  let showScene = false;
 
   const loadScene = () => {
     const app = new Application(canvas);
-    // app.load('https://prod.spline.design/Bq2oeyVUmqdzWQ0F/scene.splinecode')
-    app.load(scene)
-    // .then(() => {
-    //   const cube = app.findObjectByName("Group");
-    //   click = () => {
-    //     if (cube) {
-    //       // cube.rotation.y += -0.5;
-    //     }
-    //   };
-    // });
+    app
+      // .load("https://prod.spline.design/Bq2oeyVUmqdzWQ0F/scene.splinecode")
+      app.load(scene)
+      .then(() => {
+        showScene = true;
+        app.setVariable("previewId", sceneNo);
+      });
     click = () => {
-      app.setVariable('previewId', (++sceneNo % 3));
+      app.setVariable("previewId", ++sceneNo % 3);
     };
+
+    switchScene = (id) => {
+      console.log("switching scene", id);
+      app.setVariable("previewId", id);
+    }
   };
+
 
   onMount(() => {
     loadScene();
   });
 </script>
 
-<main>
-  <!-- ur project here -->
-  <canvas bind:this={canvas} id="canvas3d"></canvas>
-  <button on:click={click}>Next</button>
+<!-- <img src={demoScreen} alt="demoScreen"> -->
+<canvas bind:this={canvas} id="canvas3d" class:visible={showScene}></canvas>
+
+{#if showScene}  
+<main in:fade>
+  <!-- <button on:click={click}>Next</button> -->
+  <header>
+    {@html CubiqLogo }
+    <!-- <img src={cubiqLogo} alt="cubiqLogo" /> -->
+  </header>
+  <section>
+    <h1>
+      <span>Hi, I’m Kuba</span>
+      <span  class="desc"
+        >A Blablabla designer, wannabe engineer, and sometimes chicken living in
+        the Czechia</span
+      >
+    </h1>
+    <!-- <p class="space"> -->
+    <p>
+      I'm afraid I just blue myself. It's called 'taking advantage.' It's what
+      gets you ahead in life. No… but I'd like to be asked! We just call it a
+      sausage. There's only one man I've ever called a coward, and that's Brian
+      <WorkPill name="Aquad" {switchScene} id={2} />
+      Brian is my guy
+    </p>
+    <p>
+      That's why you always leave a note! I hear the jury's still out on
+      science. First place chick is hot, but has an attitude, doesn't date
+      magicians. I'm a monster. Well, what
+      <WorkPill name="XOR" {switchScene} id={1} />
+      , more stuff lol
+    </p>
+    <p>
+      We just call it a sausage. There's only one man I've ever called a coward,
+      <WorkPill name="Nothing" {switchScene} id={0} />
+      , a high-interest savings
+    </p>
+    <p>
+      Weaseling out of things is important to learn. It's what separates us from
+      the animals…except the weasel. Remember the time he ate my goldfish? And
+      you lied and said I never had goldfish. Then why did I have the bowl,
+      Bart? *Why did I have the bowl?* Lisa, vampires are make-believe, like
+      elves, gremlins, and Eskimos. I didn't get rich by signing checks. I've
+      done everything the Bible says — even the stuff that contradicts the other
+      stuff! What good is money if it can't inspire terror in your fellow man?
+      Homer no function beer well without.
+    </p>
+    <p>
+      They only come out in the night. Or in this case, the day. And here I am
+      using my own lungs like a sucker. I didn't get rich by signing checks.
+      …And the fluffy kitten played with
+      <WorkPill name="Svelte" />
+      My favorite framework srsly
+    </p>
+  </section>
 </main>
 
+{:else}
+    <h1>Loading...</h1>
+{/if}
+
+
 <style>
+  canvas {
+    aspect-ratio: none;
+    position: fixed;
+    inset: 0;
+    cursor: grab;
+    opacity: 0;
+    transition: opacity 5s;
+  }
+  canvas.visible {
+    opacity: 1;
+  }
+
+  [alt="demoScreen"] {
+    position: fixed;
+    inset: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
   main {
     flex: 1;
-    /* display: grid;
-    place-items: center;
-    height: 100vh;
-    position: relative;
-    */
-    overflow: hidden; 
+    height: max-content;
+    /* height: 100vh; */
+    padding: 6%;
+    padding-inline-end: 3%;
+
+    max-width: 45rem;
+
+    pointer-events: none;
+
+    z-index: 1;
   }
 
-  canvas {
-    /* width: 100vw; */
-    aspect-ratio: none;
-  }
-
-  button {
+  main::before {
+    content: "";
     position: fixed;
-    inset-inline: 0;
-    width: 10rem;
-    margin-inline: auto;
-    bottom: 2em;
+    inset: 0;
+    z-index: -1;
+    -webkit-backdrop-filter: blur(7px);
+    backdrop-filter: blur(7px);
+
+    background: #0008;
+    pointer-events: none;
+  }
+
+  section {
+    display: flex;
+    flex-direction: column;
+    gap: 1em;
+  }
+
+  main > * {
+    pointer-events: all;
+  }
+
+  h1 {
+    font-weight: 300;
+    font-size: 2.6rem;
+    color: var(--clr-accent);
+  }
+  h1 .desc {
+    /* font-size: 1.4rem; */
+    color: var(--clr-text);
+  }
+
+  h1 .desc::before {
+    content: "— " / "";
+  }
+
+  p {
+    color: var(--color-text-secondary);
+  }
+
+  /* #region peek */
+
+  p,
+  h1,
+  main,
+  main::before {
+    --_speed: 1s;
+    transition: var(--_speed);
+    transition-property: all;
+  }
+
+  :global(#app:has(canvas:hover) main) {
+    --opacity: 0.3;
+  }
+
+  main:has(button:hover) {
+    --opacity: 0;
+  }
+  main h1,
+  main::before,
+  p:not(:is(:hover, :focus-within)) {
+    opacity: var(--opacity, 1);
+  }
+
+  section > p {
+    position: relative;
+  }
+
+  section > p::before {
+    content: "";
+    position: absolute;
+    inset: -1em;
+    background: #0005;
+    opacity: 0;
+    transition: inherit;
+    z-index: -1;
+
+    --_spread: 1.5em;
+
+    mask: linear-gradient(
+      transparent,
+      white var(--_spread),
+      white calc(100% - var(--_spread)),
+      transparent
+    );
+
+    backdrop-filter: blur(5px);
+  }
+
+  p:has(button:hover)::before,
+  p:focus-within::before {
+    opacity: 1;
+  }
+
+  p:has(button:hover),
+  p:focus-within {  
+    color: var(--clr-accent);
+    --_speed: .3s;
+  }
+
+  /* #endregion */
+
+  .space {
+    margin-bottom: 22rem;
   }
 </style>
---
+++ SplineWithSvelte/src/lib/workPill.svelte
@@ -0,0 +1,157 @@
+<script>
+  import { ArrowUpRight, Eye } from "lucide-svelte";
+
+  export let name = "nothing";
+  export let id = 0;
+  export let switchScene = null;
+
+  const switchMe = () => {
+    if (switchScene) {
+      switchScene(id);
+    }
+  };
+</script>
+
+<button 
+  type="button" 
+  id="camera-2" 
+  class="work-pill"
+  on:mouseover={switchMe}
+  on:focus={switchMe}
+>
+  <span class="inset-span">
+    <Eye aria-hidden="true" />
+    <span class="work-pill-text">{name}</span>
+    <ArrowUpRight aria-hidden="true" />
+  </span>
+</button>
+
+<style>
+  @property --gradient-angle {
+    syntax: "<angle>";
+    initial-value: -15deg;
+    inherits: false;
+  }
+
+  @keyframes rotation {
+    0% {
+      --gradient-angle: -15deg;
+    }
+
+    100% {
+      --gradient-angle: 345deg;
+    }
+  }
+
+  .work-pill {
+    /* remove all button styles */
+    background: none;
+    border: none;
+    padding: 0;
+    font: inherit;
+
+    color: var(--clr-accent);
+    cursor: pointer;
+    border-radius: 8px;
+    
+    padding-inline: 0.5em;
+    
+    display: inline-block;
+    position: relative;
+    
+    font-weight: 600;
+    
+    --_bg-y: -0.05em;
+  }
+
+  .work-pill:before {
+    filter: blur(4px);
+    transform: translate(0, 0);
+  }
+  .work-pill:before,
+  .work-pill:after {
+    content: "";
+    border-radius: inherit;
+    background: conic-gradient(
+      from var(--gradient-angle),
+      #aaa3,
+      #aaa7,
+      #aaaf,
+      #aaa7,
+      #aaa3
+    );
+    opacity: 0.4;
+    /* height: 24px; */
+    transition: opacity 0.5s;
+    animation: 5s linear infinite rotation;
+    position: absolute;
+    inset: 0;
+    inset-block: var(--_bg-y);
+  }
+
+  .work-pill::before {
+    filter: blur(4px);
+    transform: translateZ(0);
+  }
+
+  .inset-span {
+    overflow: visible;
+  }
+
+  .inset-span:after {
+    content: "";
+    z-index: 1;
+    -webkit-backdrop-filter: blur(10px);
+    backdrop-filter: blur(10px);
+    background: #1118;
+    border-radius: 7px;
+    position: absolute;
+
+    --_inset: .075em;
+
+    inset: var(--_inset);
+    inset-block: calc(var(--_inset) + var(--_bg-y));
+  }
+
+  .work-pill-text {
+    z-index: 2;
+
+    font-size: 15px;
+    position: relative;
+  }
+
+  :global(.work-pill .lucide) {
+    height: 1em;
+    width: 1em;
+    z-index: 2;
+    position: relative;
+    margin-bottom: -0.16em;
+
+    transition: 0.15s cubic-bezier(0.4, 0, 0.2, 1);
+  }
+
+  :global(
+      .work-pill .lucide:first-of-type,
+      .work-pill:hover .lucide:last-of-type
+    ) {
+    opacity: 1;
+    margin-right: 0em;
+    transform: translateX(0px);
+  }
+
+  :global(
+      .work-pill:hover .lucide:first-of-type,
+      .work-pill .lucide:last-of-type
+    ) {
+    opacity: 0;
+    margin-right: -1em;
+    transform: translateX(-5px);
+  }
+
+  /* max-width: 2em; */
+
+  .work-pill:hover::after,
+  .work-pill:hover::before {
+    opacity: 0.8;
+  }
+</style>
